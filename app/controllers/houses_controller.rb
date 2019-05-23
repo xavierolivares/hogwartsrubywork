c4836dac1a49b61464f555c39980cef2364be34c
Xavier Olivares <xm.olivares@gmail.com>
2019-05-23 10:37:51 -0400

    pending work

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -2,6 +2,14 @@ class HousesController < ApplicationController
     def index
         @all_houses = House.all()
     end
-    def create
-        @ = House.create()
+    def new 
+        @house = House.new
+    end
+    # def create
+    #     @house = House.new(params[:house])
+    #     if @house.save
+    #       redirect_to @house
+    #     else
+    #       render "new"
+    # end
 end
